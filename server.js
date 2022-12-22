22454d6f14bb03ccfd7af13a371f3e552981acde
vishal181214 <vishal32gaykwad@gmail.com>
2022-12-22 13:16:56 +0530

    backend api completed

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import data from './data.js';
+import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
+import cors from 'cors';
+const app = express();
+
+app.use(cors())
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://vishalgai:UKkUknD3ztBhSxdV@cluster0.apnmfbt.mongodb.net/myshopee?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// converting form data into json 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get(`/api/keys/paypal`,(req,res)=>{
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb')  // sb for sandbox
+});
+
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+
+// error handler if error occured then this middleware is executed
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
